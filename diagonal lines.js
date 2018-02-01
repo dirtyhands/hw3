c81a8d2fc055634f3c60f6dc639cb4dfe0323ba1
dirtyhands <e.frazin@gmail.com>
2018-02-01 12:50:20 -0800

    Update diagonal lines.js

--- diagonal lines.js
+++ diagonal lines.js
@@ -1,7 +1,12 @@
 background (0,0,0)
-stroke(color(360, 360, 360));
+stroke(color(360, 360));
 var count = 20;
 while (count < width){
   line(10, count,count, 10);
   count = count + 10;
 }
+//var count = 20;
+//while (count < width-10){
+//  line(count, width-10, width-10, count);
+//  count = count + 10;
+//}
