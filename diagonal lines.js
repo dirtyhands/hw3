d317436245696ab592431b4ef98f37d76656827a
dirtyhands <e.frazin@gmail.com>
2018-03-08 00:29:34 -0800

    Update diagonal lines.js

--- diagonal lines.js
+++ diagonal lines.js
@@ -5,8 +5,3 @@ while (count < width){
   line(10, count,count, 10);
   count = count + 10;
 }
-//var count = 20;
-//while (count < width-10){
-//  line(count, width-10, width-10, count);
-//  count = count + 10;
-//}
